c90d3871f91d5f9055e940f7cb3360145ddb95f4
魏遇卿 <358614401@qq.com>
2023-07-22 21:50:40 +0800

    update

---
+++ List/listCode.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 35861 on 2023/7/22.
+//
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+
+struct Book {
+    int id;
+    string name;
+    double price;
+};
